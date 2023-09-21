aaf3a49a71c0c198d2ebdcb918fe9b859bd3b550
Eliud On KFA Desktop <eliudfromkenya@gmail.com>
2023-09-21 11:39:01 +0300

    wert werterter

--- LevenshteinDistanceAlgorithm/ConnectionObject.cs
+++ LevenshteinDistanceAlgorithm/ConnectionObject.cs
@@ -14,14 +14,14 @@ public class ConnectionObject
     public DBModel MaliplusDbModel { get => maliplusDbModel; set => maliplusDbModel = value; }
     public DBModel ZanasDbModel { get => zanasDbModel; set => zanasDbModel = value; }
 
-    //public IBM.Data.DB2.Core.DB2Connection MaliplusConnection => new IBM.Data.DB2.Core.DB2Connection($"Server={MaliplusDbModel.Host}:{MaliplusDbModel.Port};Database={MaliplusDbModel.Database};UID={MaliplusDbModel.User};PWD={MaliplusDbModel.Password};");
-    //public IBM.Data.DB2.Core.DB2Connection ZanasConnection => new IBM.Data.DB2.Core.DB2Connection($"Server={ZanasDbModel.Host}:{ZanasDbModel.Port};Database={ZanasDbModel.Database};UID={ZanasDbModel.User};PWD={ZanasDbModel.Password};");
+	//public IBM.Data.DB2.Core.DB2Connection MaliplusConnection => new IBM.Data.DB2.Core.DB2Connection($"Server={MaliplusDbModel.Host}:{MaliplusDbModel.Port};Database={MaliplusDbModel.Database};UID={MaliplusDbModel.User};PWD={MaliplusDbModel.Password};");
+	//public IBM.Data.DB2.Core.DB2Connection ZanasConnection => new IBM.Data.DB2.Core.DB2Connection($"Server={ZanasDbModel.Host}:{ZanasDbModel.Port};Database={ZanasDbModel.Database};UID={ZanasDbModel.User};PWD={ZanasDbModel.Password};");
 
 
-    private DBModel mySQLSubServerDbModel = new() { Database = "kfa_sub_systems", Host = "192.168.1.240", Password = "*654321.0kfa#", Port = "50000", Schema = "KFALTD", User = "remote_user" };
-    private DBModel mySQLDbModel = new() { Database = "kfa_sub_systems", Host = "127.0.0.1", Password = "654321", Port = "3306", Schema = "ZANAS", User = "root" };
-   
-     public DBModel MySQLSubServerDbModel { get => mySQLSubServerDbModel; set => mySQLSubServerDbModel = value; }
+	private DBModel mySQLSubServerDbModel = new() { Database = "kfa_sub_systems", Host = "192.168.1.239", Password = "isd@KFA2023#", Port = "3306", Schema = "KFALTD", User = "remoteuser" };
+	private DBModel mySQLDbModel = new() { Database = "kfa_sub_systems", Host = "127.0.0.1", Password = "654321", Port = "3306", Schema = "ZANAS", User = "root" };
+
+	public DBModel MySQLSubServerDbModel { get => mySQLSubServerDbModel; set => mySQLSubServerDbModel = value; }
     public DBModel MySQLDbModel { get => mySQLDbModel; set => mySQLDbModel = value; }
 
     public MySqlConnection MySQLSubServerConnection => new($@"server={MySQLSubServerDbModel.Host};port={MySQLSubServerDbModel.Port};database={MySQLSubServerDbModel.Database};user={MySQLSubServerDbModel.User};password={MySQLSubServerDbModel.Password};ConvertZeroDateTime=True;AllowUserVariables=True;");
